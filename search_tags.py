fa4addef4fd6ee5e7b142d3fb53f04a6dae46bfe
Albert Garcia <alberto.garcia@oculus.com>
2017-10-23 15:36:09 -0700

    add tag searching Python script with terminal colors

--- search_tags.py
+++ search_tags.py
@@ -1,15 +1,31 @@
 import argparse
+import bcolors as bc
+import frontmatter
+import glob
+import io
 import os
 
 parser = argparse.ArgumentParser(description='Tag searching for documents with YAML headers')
 
-parser.add_argument('dir', help='The directory to search for files')
+parser.add_argument('path', help='The directory to search for files')
 parser.add_argument('--tags', nargs='+')
 
 args = parser.parse_args()
 
-directory = args.dir
+path = args.path
 tags = args.tags
 
-print directory
-print tags
+print 'Searching in ', path, 'for tags: ', tags
+
+for fname in glob.glob(path + '/*.md'):
+	with io.open(fname, 'r') as f:
+		header = frontmatter.load(f)
+		file_tags = header['tags']
+
+		common_tags = []
+		for tag in tags:
+			if tag in file_tags:
+				common_tags.append(tag)
+
+		if len(common_tags) > 0:
+			print "{}{}{} : {}".format(bc.BOLD, fname, bc.ENDC, common_tags)
